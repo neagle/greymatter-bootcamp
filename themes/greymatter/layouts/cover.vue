443a6b33bc04ba9dfd69c6167e9623b29f97dbaf
Nate Eagle <nate@nateeagle.com>
2023-08-07 11:31:16 -0400

    beginning of greymatter theme

---
+++ themes/greymatter/layouts/cover.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="slidev-layout cover">
+    <div class="my-auto w-full">
+      <img :src="logoSrc" class="logo" />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import logoSrc from '../src/logos/GM_logo_Bl.png'
+
+export default {
+  data() {
+    return {
+      logoSrc
+    }
+  }
+}
+</script>
